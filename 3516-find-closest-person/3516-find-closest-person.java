f22c747a010afe11fd8559338c822825bc1c60a4
Anirudh Sharma <anirudhsharma73093@gmail.com>
2026-08-02 17:39:15 +0530

    Time: 1 ms (93.77%), Space: 42.5 MB (81.38%) - LeetHub

--- 3516-find-closest-person/3516-find-closest-person.java
+++ 3516-find-closest-person/3516-find-closest-person.java
@@ -3,7 +3,7 @@ class Solution {
         int a=Math.abs(x-z);
         int b=Math.abs(y-z);
         if(a==b)return 0;
-        else if(a>b)return 2;
+        if(a>b)return 2;
         else return 1;
     }
 }
